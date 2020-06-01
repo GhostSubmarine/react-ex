d34e59bde778bc5792abba0451819ba33e6cf922
Li Xinxin <1073944897@qq.com>
2020-06-01 16:10:13 +0800

    react 项目初始化

---
+++ src/dev.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0.0
+ * @Author: Lixinxin
+ * @Date: 2020-06-01 15:26:17
+ * @LastEditors: Lixinxin
+ * @LastEditTime: 2020-06-01 15:27:14
+ */ 
+if(module.hot){
+    module.hot.accept(err=>{
+        if(err){
+            console.log('Cannot apply HMR update.',err);
+        }
+    })
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0.0
+ * @Author: Lixinxin
+ * @Date: 2020-06-01 14:44:28
+ * @LastEditors: Lixinxin
+ * @LastEditTime: 2020-06-01 14:50:56
+ */ 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>Hello Webpack!!!</div>,
+    document.getElementById('root')
+)
